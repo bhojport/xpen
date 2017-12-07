dd7f4540c258a5f0d85f4b990e81dd7e169ba78d
Bhojendra Rauniyar <bhojport@gmail.com>
2017-12-07 21:07:25 +0545

    Linking css from index.html. Note: Currently, it will only work in live server as it depends on "node server.js" using "npm start" since npm start is not configured locally.

--- server.js
+++ server.js
@@ -2,6 +2,8 @@
 const express = require('express');
 const app = express();
 
+app.use('/bootstrap-css', express.static(__dirname + '/node_modules/bootstrap/dist/css/'));
+
 // Serve only the static files form the dist directory
 app.use(express.static(__dirname + '/dist'));
 
